ac2aefcb3b66e877e3209c4302946a2a8ae10325
perymerdeka <ferilukmansyah@gmail.com>
2026-01-13 23:10:04 +0700

    Fig Prisma

--- api/prisma.config.ts
+++ api/prisma.config.ts
@@ -2,12 +2,5 @@ import path from 'node:path';
 import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-    earlyAccess: true,
     schema: path.join(__dirname, 'prisma', 'schema.prisma'),
-    migrate: {
-        // Database URL for Prisma Migrate CLI commands
-        async url() {
-            return process.env.DATABASE_URL || '';
-        },
-    },
 });
